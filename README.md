f7c1d90d1f95a9df75ea0c6c2cefa5813878c2d4
Ji Seung Gu <tmdrn457@naver.com>
2020-04-26 17:54:39 +0900

    Added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello World 
+
+* First README.md
--- src/main/java/com/example/demo/AuthorizationConfig.java
+++ src/main/java/com/example/demo/AuthorizationConfig.java
@@ -11,11 +11,5 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("some_client_id")
-                .secret("some_client_secret")
-                .scopes("read:current_user", "read:users")
-                .authorizedGrantTypes("client_credentials");
     }
-
 }
